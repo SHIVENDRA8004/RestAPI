f3d6dfdf3f8a1fba1b6b7ef4dcd3507fd75fc785
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-03 18:01:40 +0530

    Set a env variable

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-app.listen(3000, () => {
-  console.log("Listening Started");
-});
 app.get("/", (req, res) => {
   res.send("<h1>In / Path middleware</h1>");
 });
 app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
+});
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening Started on port ${port}`);
 });
