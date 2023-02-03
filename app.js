e952751bcd7ecdf98ef12ea6f7590e241131ecb6
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-03 17:35:20 +0530

    Installed nodemon

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-const http = require("http");
-const { json } = require("stream/consumers");
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("<h1>Hiii</h1>");
-    res.end();
-  }
-  if (req.url === "/api/course") {
-    res.write(JSON.stringify([1, 3, 4]));
-    res.end();
-  }
-});
-server.listen(3000, () => {
+const express = require("express");
+const app = express();
+app.listen(3000, () => {
   console.log("Listening Started");
 });
