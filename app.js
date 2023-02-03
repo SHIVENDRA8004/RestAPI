6adbb9317dd5f33e0563805261580d9108c40adc
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-03 16:37:17 +0530

    Created http server

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    res.send("<h1>Hii</h1>");
+  if (req.url === "/") {
+    res.write("<h1>Hiii</h1>");
     res.end();
   }
+  if (req.url === "/api/course") {
+    res.write("<h1>Hiii</h1>");
+    res.end();
+  }
+});
+server.listen(3000, () => {
+  console.log("Listening Started");
 });
