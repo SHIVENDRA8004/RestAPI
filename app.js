88b732a72d1cbdd6aa95d812cb1e01c3664c80ba
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-03 18:48:19 +0530

    Added comments and make code neat and added middleware for a single course

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+// Requiring All Important Modules
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => {
-  res.send("<h1>In / Path middleware</h1>");
-});
-app.get("/api/courses", (req, res) => {
-  res.send([1, 2, 3]);
-});
+
+// Set the Environment Variable for port no.
 const port = process.env.PORT || 3000;
+
+// Listen Request On Port
 app.listen(port, () => {
   console.log(`Listening Started on port ${port}`);
+});
+
+// MiddleWare for route '/'
+app.get("/", (req, res) => {
+  res.send("Home page");
+});
+
+// MiddleWare for all courses
+app.get("/api/courses", (req, res) => {
+  res.send("All Courses");
+});
+
+// MiddleWare for a single course
+app.get("/api/courses/:id", (req, res) => {
+  res.send("Single Course of id " + req.params.id);
 });
