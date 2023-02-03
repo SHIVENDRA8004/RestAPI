8a6de16d1fef7bf01c29081006a94e074d2f75f4
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-03 17:24:23 +0530

    Setting another message

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const http = require("http");
+const { json } = require("stream/consumers");
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.write("<h1>Hiii</h1>");
     res.end();
   }
   if (req.url === "/api/course") {
-    res.write("<h1>Hiii</h1>");
+    res.write(JSON.stringify([1, 3, 4]));
     res.end();
   }
 });
