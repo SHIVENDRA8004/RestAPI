4f4965e44e3d82b309de51fc798f1bd4c7ba530a
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-05 13:37:31 +0530

    Input validation done

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.post("/api/courses", (req, res) => {
   };
   const result = Joi.validate(req.body, schema);
   if (result.error) {
-    res.status(400).send(result.error.details[0]);
+    res.status(400).send(result.error.details[0].message);
     return;
   }
   const course = {
