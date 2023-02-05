1ee9d2f241e74275347fcf131d0d367e16327f2c
SHIVENDRA8004 <jat13632@gmail.com>
2023-02-05 15:36:31 +0530

    Put Request middle ware created and copied the repeated code

--- app.js
+++ app.js
@@ -59,3 +59,21 @@ app.post("/api/courses", (req, res) => {
   courses.push(course);
   res.send(course);
 });
+
+// Middleware for Put Request
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res
+      .status(404)
+      .send(`Hey the Course with id ${req.params.id} did'nt exists`);
+  }
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+});
